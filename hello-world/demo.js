0b798836bbea0bdb0f191b2256efbdaa8c0b1b5b
Noe Lutz <noelutz@google.com>
2017-08-01 14:32:58 -0700

    pull out recipes

--- hello-world/demo.js
+++ hello-world/demo.js
@@ -17,65 +17,26 @@ const {
   workerPecFactory,
 } = window.Arcs;
 
-let recipes = [
-{
-  name: "Hello, World! Hello, you!",
-  particles: [
-    {
-      name: "Greeting",
-      constrain: {
-        "person": "person"
-      }
-    },
-    {
-      name: "HelloWorld",
-      constrain: {
-        "person": "person"
-      }
-    },
-    {
-      name: "Choose",
-      constrain: {
-        "singleton": "person"
-      }
-    }
-  ]
-}];
-
-let stages = [{
-  recipes: [
-    recipes[0],
-  ]
-}];
-
-let db = {
-  people: [
-    {
-      name: "Claire"
-    }
-  ],
-};
-
-function ContextFactory({loader, pecFactory, slotComposer}) {
-  // uber arc
-  let pageArc = new Arc({loader, id: 'pageArc'});
-  // bootstrap data context
+function ContextFactory({ loader, pecFactory, slotComposer }) {
+  // Some Arc that provides the peron entity. This could be a system
+  // Arc or a page that reveals the user's identity.
+  let pageArc = new Arc({ loader, id: 'pageArc' });
   let Person = loader.loadEntity('Person');
   // TODO(sjmiles): empirically, views must exist before committing Entities
   let personView = pageArc.createView(Person.type.viewOf(), 'peopleFromWebpage');
-  // commit entities
-  pageArc.commit(db.people.map(p => new Person(p)));
-  // demo arc
-  let arc = new Arc({id: 'demo', loader, pecFactory, slotComposer});
+  // Commit a single Person entity to the arc.
+  pageArc.commit([new Person({name: "Claire"})]);
+  // Main Arc that represents the demo.
+  let arc = new Arc({ id: 'demo', loader, pecFactory, slotComposer });
   arc.createView(Person.type, 'personSlot');
   arc.mapView(personView);
   // TODO(sjmiles): boilerplate? not needed until we are rendering particles (arc not pageArc)?
   systemParticles.register(loader);
-  // your context objects
-  return {arc};
+  return arc;
 }
 
-let template = Object.assign(document.createElement('template'), {innerHTML: `
+let template = Object.assign(document.createElement('template'), {
+  innerHTML: `
 
 <style>
   :host {
@@ -103,7 +64,8 @@ let template = Object.assign(document.createElement('template'), {innerHTML: `
 </auto-tabs>
 <suggestions-element></suggestions-element>
 
-`.trim()});
+`.trim()
+});
 
 class DemoFlow extends DemoBase {
   get template() {
@@ -111,13 +73,14 @@ class DemoFlow extends DemoBase {
   }
   didMount() {
     let root = '../';
-    let {arc} = ContextFactory({
+    let arc = ContextFactory({
       loader: new BrowserLoader(root),
       pecFactory: workerPecFactory.bind(null, root),
       slotComposer: new SlotComposer(this.$('[particle-container]'))
     });
     this.arc = arc;
-    this.stages = stages;
+    // This demo has a single stage.
+    this.stages = [{ recipes: [window.recipes.helloWorld] }];
     this.suggestions = this.$('suggestions-element');
     this.suggestions.arc = arc;
     this.suggestions.callback = this.nextStage.bind(this);
